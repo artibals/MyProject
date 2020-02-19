7f05a69e2b3b01cf92b98c4a24814ec3677c1c18
Orest Andrushkiv <artibals@gmail.com>
2020-02-20 00:02:02 +0200

    new js, css changes

---
+++ js/accordeon.js
@@ -0,0 +1,15 @@
+const list = document.getElementById('accordeon')
+list.addEventListener('click', function openAccordeon(event){
+   
+   const openClass = 'acc-li-open'
+    if (event.target.classList.contains('acc-opener')) {
+        let content = event.target.parentNode
+        
+        if (content.classList.contains(openClass)) {
+            content.classList.remove(openClass)
+        } else {
+            content.classList.add(openClass);
+        }
+    }
+    
+})
